c7495336a6ee7a4a77b4256270dc46e7062a95fa
nuna <nuna@nunas-MacBook-Pro.local>
2023-02-02 23:17:28 -0800

    added auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
 import {
   withAuthenticator,
@@ -11,12 +9,12 @@ import {
   Card,
 } from "@aws-amplify/ui-react";
 
-function App({signOut}) {
+function App({ signOut }) {
   return (
     <View className="App">
       <Card>
-          <Image src={logo} className="App-logo" alt="logo" />
-          <Heading level={1}> DO YOU SEE MY AMPLIFY APP</Heading>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
       </Card>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
